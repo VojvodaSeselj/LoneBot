be7f40ff08af38580af40fc5886f87f1288e7526
Djordje Colovic <djordje2001.oeo@gmail.com>
2020-02-19 21:00:52 +0100

    Update verify.js

--- commands/member/verify.js
+++ commands/member/verify.js
@@ -16,9 +16,9 @@ module.exports = {
   let verifychannel = guild.Verify.Channel
   if (message.channel.name !== verifychannel) return message.delete();
   if (!message.member.roles.some(r=>[guild.Verify.VerifyRole].includes(r.name)) && message.member.roles.some(r=>[guild.Verify.VerifiedRole].includes(r.name))) return message.reply("You are already verified!").then(m => m.delete(3000))
-  if (guild.Verify.VerifiedRole === "" || guild.Verify.VerifyRole === "" || guild.Verify.Channel === "" || guild.Verify.LogsChannel === "") return message.delete() && message.reply(`Verify is not set up correctly.To see how to set it up do **${guild.Prefix}botconfig**!`).then(m => m.delete(3000));
-  if (guild.Verify.Enabled !== "true") return message.delete() && message.reply(`Verify is disabled.To enable it do **${guild.Prefix}enableverify**!`).then(m => m.delete(3000));
-    if (guild.Verify.Enabled === "true" && guild.Verify.VerifiedRole !== "" && guild.Verify.VerifyRole !== "" && guild.Verify.Channel !== "" && guild.Verify.LogsChannel !== "") {
+  if (guild.Verify.Enabled === true || guild.Verify.VerifiedRole === "" || guild.Verify.VerifyRole === "" || guild.Verify.Channel === "" || guild.Verify.LogsChannel === "") return message.delete() && message.reply(`Verify is not set up correctly.To see how to set it up do **${guild.Prefix}botconfig**!`).then(m => m.delete(3000));
+  if (guild.Verify.Enabled !== true) return message.delete() && message.reply(`Verify is disabled.To enable it do **${guild.Prefix}enableverify**!`).then(m => m.delete(3000));
+    if (guild.Verify.Enabled === true && guild.Verify.VerifiedRole !== "" && guild.Verify.VerifyRole !== "" && guild.Verify.Channel !== "" && guild.Verify.LogsChannel !== "") {
     let verifyRole = message.guild.roles.find(role => role.name === guild.Verify.VerifyRole);
     let verifiedRole = message.guild.roles.find(role => role.name === guild.Verify.VerifiedRole);
     if (!verifiedRole) return message.reply("Couldn't find that role");
