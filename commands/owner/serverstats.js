d6b761fc2ea8bcc0c455013b2ed3de4199397f06
Djordje Colovic <djordje2001.oeo@gmail.com>
2020-02-19 20:03:35 +0100

    Update serverstats.js

--- commands/owner/serverstats.js
+++ commands/owner/serverstats.js
@@ -25,13 +25,13 @@ module.exports = {
 		if (opcija.includes("on")) {
       let emoji = message.emojis.find(emoji => emoji.name === "bar_chart");
 			const parent = await message.guild.createCategory(`${emoji}Server Stats${emoji}`)
-      const botc = await message.guild.createChannel(`Bot Count: ${message.guild.members.filter(member => member.user.bot).size}`)
+      const botc = await message.guild.createChannel(`Bot Count: ${message.members.filter(member => member.user.bot).size}`)
       botc = await botc.setParent(parent)
-      const userc = await message.guild.createChannel(`User Count: ${message.guild.members.filter(member => !member.user.bot).size}`)
+      const userc = await message.guild.createChannel(`User Count: ${message.members.filter(member => !member.user.bot).size}`)
       userc = await userc.setParent(parent)
-      const channelc = await message.guild.createChannel(`Channel Count: ${message.guild.channels.size}`)
+      const channelc = await message.guild.createChannel(`Channel Count: ${message.channels.size}`)
       channelc = await channelc.setParent(parent)
-      const rolec = await message.guild.createChannel(`Role Count: ${message.guild.roles.size}`)
+      const rolec = await message.guild.createChannel(`Role Count: ${message.roles.size}`)
       rolec = await rolec.setParent(parent)
 
 		} else if (opcija.includes("off")) {
