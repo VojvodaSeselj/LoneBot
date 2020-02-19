861ad87dd2ff040e605f869366fdb94256f936cc
Djordje Colovic <djordje2001.oeo@gmail.com>
2020-02-19 20:06:17 +0100

    Update serverstats.js

--- commands/owner/serverstats.js
+++ commands/owner/serverstats.js
@@ -23,23 +23,22 @@ module.exports = {
     if (!args[0]) return;
     let opcija = args[0].toLowerCase();
 		if (opcija.includes("on")) {
-      let emoji = message.emojis.find(emoji => emoji.name === "bar_chart");
-			const parent = await message.guild.createCategory(`${emoji}Server Stats${emoji}`)
-      const botc = await message.guild.createChannel(`Bot Count: ${message.members.filter(member => member.user.bot).size}`)
+			const parent = await message.guild.createCategory(`:bar_chart:Server Stats:bar_chart:`)
+      const botc = await message.guild.createChannel(`Bot Count: ${message.guild.members.filter(member => member.user.bot).size}`)
       botc = await botc.setParent(parent)
-      const userc = await message.guild.createChannel(`User Count: ${message.members.filter(member => !member.user.bot).size}`)
+      const userc = await message.guild.createChannel(`User Count: ${message.guild.members.filter(member => !member.user.bot).size}`)
       userc = await userc.setParent(parent)
-      const channelc = await message.guild.createChannel(`Channel Count: ${message.channels.size}`)
+      const channelc = await message.guild.createChannel(`Channel Count: ${message.guild.channels.size}`)
       channelc = await channelc.setParent(parent)
-      const rolec = await message.guild.createChannel(`Role Count: ${message.roles.size}`)
+      const rolec = await message.guild.createChannel(`Role Count: ${message.guild.roles.size}`)
       rolec = await rolec.setParent(parent)
 
 		} else if (opcija.includes("off")) {
-      message.guild.deleteCategory(parent)
-      message.guild.deleteChannel(botc)
-      message.guild.deleteChannel(userc)
-      message.guild.deleteChannel(channelc)
-      message.guild.deleteChannel(rolec)
+      message.guild.deleteCategory(parent.id)
+      message.guild.deleteChannel(botc.id)
+      message.guild.deleteChannel(userc.id)
+      message.guild.deleteChannel(channelc.id)
+      message.guild.deleteChannel(rolec.id)
     }
 	}
 };
