6decfdca84f492b74e2c805eabb5b5a756a1e57d
Djordje Colovic <djordje2001.oeo@gmail.com>
2020-02-20 19:24:58 +0100

    Update color.js

--- commands/fun/color.js
+++ commands/fun/color.js
@@ -2,6 +2,7 @@ const { RichEmbed } = require("discord.js");
 const { promptMessage, getMember } = require("../../functions.js");
 const { stripIndents } = require("common-tags");
 const Guild = require("../../models/guild.js");
+const boje["light pink", "pink", "dark pink", "light orange", "orange", "dark orange", "light yellow", "yellow", "dark yellow", "light green", "green", "dark green", "light blue", "blue", "dark blue", "light purple", "purple", "dark purple", "light red", "red", "dark red"]
 
 module.exports = {
     name: "color",
@@ -19,13 +20,13 @@ module.exports = {
       return message.reply("You need to supply color your want!").then(m => m.delete(5000));
     }
     let color = args.slice(0).join(" ")
-    if(!["light pink", "pink", "dark pink", "light orange", "orange", "dark orange", "light yellow", "yellow", "dark yellow", "light green", "green", "dark green", "light blue", "blue", "dark blue", "light purple", "purple", "dark purple", "light red", "red", "dark red"].includes(args[0])) return message.reply("You can only choose from colors above!");
+    if(!boje.includes(args[0])) return message.reply("You can only choose from colors above!");
 
     const role = message.guild.roles.find((r) => r.name === color);
     if (!role) {
       return message.reply("You can only choose from colors above!");
     }
-    if (message.author.roles.has(role.id)) {
+    if (message.author.roles.some(boje.id)) {
       return message.reply(`${rUser} you already got this color!`)
     }
 
